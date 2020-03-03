28fd5d3884c1a3f24278bfadc14b060cbd60af36
go4victory <z5102326@zmail.unsw.edu.au>
2020-03-03 15:59:52 +1100

    Updates

---
+++ precache-manifest.74dc649ba6c05375763c17f817f5e2c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023f63604c942b3a74fdc018b302a818",
+    "url": "/go4victory.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e013b6264cf378fc5956",
+    "url": "/go4victory.github.io/monsters-rolodex/static/css/main.8ebbf251.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/go4victory.github.io/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/go4victory.github.io/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e013b6264cf378fc5956",
+    "url": "/go4victory.github.io/monsters-rolodex/static/js/main.84184834.chunk.js"
+  },
+  {
+    "revision": "4410f002bc8d1e1dd94d",
+    "url": "/go4victory.github.io/monsters-rolodex/static/js/runtime-main.3461790a.js"
+  }
+]);
