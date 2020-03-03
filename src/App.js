9a869b3a717a5f75eebdd55009028cf71759bb5e
go4victory <z5102326@zmail.unsw.edu.au>
2020-03-03 18:04:54 +1100

    redux applied to the app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 import CardList from './components/card-list/card-list';
 import SearchBox from './components/search-box/search-box';
-class App extends React.Component {
-  state = {
-    monsters: [],
-    searchField: ''
+
+import { setSearchField, requestRobots } from './actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   };
+};
+const mapDispatchToPros = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  };
+};
+class App extends React.Component {
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
+    this.props.onRequestRobots();
   }
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
+    // const { monsters: robots } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
+    const filteredMonsters = robots.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
     return (
       <div className="App">
         <h1>monster</h1>
         <SearchBox
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={onSearchChange}
           placeholder="search monsters"
         ></SearchBox>
-        <CardList monsters={filteredMonsters}></CardList>
+        {isPending ? (
+          <h2>loading...</h2>
+        ) : (
+          <CardList monsters={filteredMonsters}></CardList>
+        )}
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToPros)(App);
